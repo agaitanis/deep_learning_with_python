edc63469c3a81c46c3785c93afcb9c0512a83f8f
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-03-04 16:32:34 +0200

    Continue with imdb.py

--- imdb.py
+++ imdb.py
@@ -7,6 +7,7 @@ Deep Learning with Python by Francois Chollet
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
+import matplotlib.pyplot as plt
 import numpy as np
 
 def vectorize_sequences(sequences, dimension=10000):
@@ -27,6 +28,34 @@ y_test = np.asarray(test_labels).astype('float32')
 
 # The model definition
 model = Sequential()
-model.add(Dense(16, activation='relu'), input_shape=(10000,))
+model.add(Dense(16, activation='relu', input_shape=(10000,)))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+
+# Compiling the model
+model.compile(optimizer='rmsprop',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+# Setting aside a validation set
+x_val = x_train[:10000]
+partial_x_train = x_train[:10000]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[:10000]
+
+# Training your model
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+# Plotting the training and validation loss
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.show()
