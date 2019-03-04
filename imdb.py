07cc35f9a1f8ef1370a28b8990b3563025b3d14e
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-03-04 21:02:58 +0200

    Finished imdb.py

--- imdb.py
+++ imdb.py
@@ -39,10 +39,10 @@ model.compile(optimizer='rmsprop',
 
 # Setting aside a validation set
 x_val = x_train[:10000]
-partial_x_train = x_train[:10000]
+partial_x_train = x_train[10000:]
 
 y_val = y_train[:10000]
-partial_y_train = y_train[:10000]
+partial_y_train = y_train[10000:]
 
 # Training your model
 history = model.fit(partial_x_train,
@@ -55,7 +55,44 @@ history = model.fit(partial_x_train,
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
-
 epochs = range(1, len(loss_values) + 1)
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
+
+fig1 = plt.figure()
+ax1 = fig1.gca()
+ax1.plot(epochs, loss_values, 'bo', label='Training loss')
+ax1.plot(epochs, val_loss_values, 'r', label='Validation loss')
+ax1.set_title('Training and validation loss')
+ax1.set_xlabel('Epochs')
+ax1.set_ylabel('Loss')
+ax1.legend()
+
+# Plotting the training and validation accuracy
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+fig2 = plt.figure()
+ax2 = fig2.gca()
+ax2.plot(epochs, acc_values, 'bo', label='Training accuracy')
+ax2.plot(epochs, val_acc_values, 'r', label='Validation accuracy')
+ax2.set_title('Training and validation accuracy')
+ax2.set_xlabel('Epochs')
+ax2.set_ylabel('Accuracy')
+ax2.legend()
+
 plt.show()
+
+# Retraining a model from scratch
+model = Sequential()
+model.add(Dense(16, activation='relu', input_shape=(10000,)))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
+print(results)
+
+model.predict(x_test)
