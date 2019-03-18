a5caeb8887cd45c5034eb783ef61bea131e10a29
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-03-18 18:16:05 +0200

    Update boston_housing.py

--- boston_housing.py
+++ boston_housing.py
@@ -5,8 +5,30 @@ Deep Learning with Python by Francois Chollet
 3.6 Predicting house prices: a regression example
 """
 from keras.datasets import boston_housing
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
 
 # Loading the Boston housing dataset
 (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
 # Normalizing the data
+m = np.mean(train_data, axis=0)
+s = np.std(train_data, axis=0)
+train_data = (train_data - m)/s
+test_data = (test_data - m)/s
+
+# Model definition
+def build_model():
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_shape=(train_data.shape[1], )))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='rmsprop',
+                  loss='mse',           # Mean Square Error
+                  metrics=['mae'])      # Mean Absolute Error
+    return model
+
+# K-fold validation
+k = 4
