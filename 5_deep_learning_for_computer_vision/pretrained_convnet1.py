0077a3ebac9617280f1bf022d669f9969378d4ea
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-04-08 08:19:19 +0300

    Pretrained convnet

---
+++ 5_deep_learning_for_computer_vision/pretrained_convnet1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Deep Learning with Python by Francois Chollet
+5. Deep learning for computer vision
+5.3 Using a pretrained convnet
+Fast feature extraction without data augmentation
+"""
+# Instantiating the VGG16 convolutional base
+from keras.applications import VGG16
+
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
+conv_base.summary()
+
+
+# Extracting features using the convolutional base
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = 'cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+            directory,
+            target_size=(150, 150),
+            batch_size=batch_size,
+            class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) *batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+
+# Defining and training the densely connected classifier
+
+
+
+
+
+
+
+
+
