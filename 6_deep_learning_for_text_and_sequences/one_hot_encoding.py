8988165c9d1c878687bde639d07816dec1090bc6
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-05-05 19:20:50 +0300

    Create one_hot_encoding.py

---
+++ 6_deep_learning_for_text_and_sequences/one_hot_encoding.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Deep Learning with Python by Francois Chollet
+6. Deep learning for text and sequences
+6.1 Working with text data
+6.1.1 One-hot encoding of words and characters
+"""
+# Word-level one-hot encoding (toy example)
+import numpy as np
+
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+
+token_index = {}
+for sample in samples:
+    for word in sample.split():
+        if word not in token_index:
+            token_index[word] = len(token_index) + 1
+
+max_length = 10
+
+results = np.zeros(shape=(len(samples),
+                   max_length,
+                   max(token_index.values()) + 1))
+
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split()))[:max_length]:
+        index = token_index.get(word)
+        results[i, j, index] = 1.
+        
+
+# Character-level one-hot encoding (toy example)
+import string
+
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+characters = string.printable
+token_index = dict(zip(characters, range(1, len(characters) + 1)))
+
+max_length = 50
+results = np.zeros(shape=(len(samples),
+                   max_length,
+                   max(token_index.values()) + 1))
+
+for i, sample in enumerate(samples):
+    for j, character in enumerate(sample):
+        index = token_index.get(character)
+        results[i, j, index] = 1.
+
+
+# Using Keras for word-level one-hot encoding
+from keras.preprocessing.text import Tokenizer
+
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+
+tokenizer = Tokenizer(num_words=1000)
+tokenizer.fit_on_texts(samples)
+
+sequences = tokenizer.texts_to_sequences(samples)
+one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+
+# Word-level one-hot encoding with hashing trick (toy example)
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+
+dimensionality = 1000
+max_length = 10
+
+results = np.zeros((len(samples), max_length, dimensionality))
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split()))[:max_length]:
+        index = abs(hash(word)) % dimensionality
+        results[i, j, index] = 1.
