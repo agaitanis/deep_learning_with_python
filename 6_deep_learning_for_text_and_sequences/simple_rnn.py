6c9614fe5a5fb835903750acd805c6ac8f2c56e4
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-05-08 19:24:22 +0300

    Create simple_rnn.py

---
+++ 6_deep_learning_for_text_and_sequences/simple_rnn.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Deep Learning with Python by Francois Chollet
+6. Deep learning for text and sequences
+6.2 Understanding recurrent neural networks
+"""
+# Numpy implementation of a simple RNN
+import numpy as np
+
+timesteps = 100
+input_features = 32
+output_features = 64
+
+inputs = np.random.random((timesteps, input_features))
+
+state_t = np.zeros((output_features,))
+
+W = np.random.random((output_features, input_features))
+U = np.random.random((output_features, output_features))
+b = np.random.random((output_features,))
+
+successive_outputs = []
+
+for input_t in inputs[:1]:
+    output_t = np.tanh(np.dot(W, input_t) + np.dot(U, state_t) + b)
+    successive_outputs.append(output_t)
+    state_t = output_t
+
+final_output_sequence = np.asarray(successive_outputs)
