d728c3faf11bfdadc53151ea15b893b052fd06b8
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-05-09 08:21:13 +0300

    Keras SimpleRNN and LSTM

---
+++ 6_deep_learning_for_text_and_sequences/keras_rnn.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Deep Learning with Python by Francois Chollet
+6. Deep learning for text and sequences
+6.2 Understanding recurrent neural networks
+6.2.1 A recurrent layer in Keras
+"""
+# Preparing the IMDB data
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 10000
+maxlen = 500
+batch_size = 32
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+
+# Training the model with Embedding and SImpleRNN layers
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN, Dense
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+
+
+# Plotting results
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Traning and validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Traning and validation loss')
+plt.legend()
+
+plt.show()
