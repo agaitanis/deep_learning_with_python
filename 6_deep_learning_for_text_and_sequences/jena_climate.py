db91b42880c3b33332b38b9593f6a22c92feaa5b
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-05-15 20:41:06 +0300

    Update jena_climate.py

--- 6_deep_learning_for_text_and_sequences/jena_climate.py
+++ 6_deep_learning_for_text_and_sequences/jena_climate.py
@@ -53,13 +53,37 @@ float_data /= std
 
 
 # Generator yielding timeseries samples and their targets
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    
+    while True:
+        if shuffle:
+            rows = np.random.randint(min_index + lookback, max_index,
+                                     size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+        
+        samples = np.zeros((batch_size, lookback//step, data.shape[-1]))
+        targets = np.zeros((batch_size,))
+        
+        for j, row in enumerate(rows):
+            indices = range(row - lookback, row, step)
+            samples[j] = data[indices]
+            targets[j] = data[row + delay][1]
+        
+        return samples, targets
 
 
-
-
-
-
-
-
+# Preparing the training, validation and test generators
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
 
 
